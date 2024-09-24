51bd9261b8405e65180dcf2269d8f94ba3cb4bd0
Saurabh Sapkal <saurabhsapkal16@gmail.com>
2024-09-24 15:10:13 +0530

    completed the search bar logic and the profile page added logout to profile page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,11 +8,12 @@ import EmptyState from "../../components/EmptyState";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
   const { data: latestPosts } = useAppwrite(getLatestPosts);
-
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const [refreshing, setRefreshing] = useState(false)
   const onRefresh = async () => {
     setRefreshing(true);
@@ -33,10 +34,10 @@ const Home = () => {
             <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  John Doe
+                  {user?.username}
                 </Text>
               </View>
 
